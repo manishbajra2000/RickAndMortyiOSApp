1aaccdba2bdf507e00d6f5fcbdfea8b6170b3061
manishbajra2000 <mantu.bajra2000@gmail.com>
2024-09-10 20:27:43 +0545

    Created Rick and Morty project

--- RickAndMorty/Controllers/Core/RMTabBarController.swift
+++ RickAndMorty/Controllers/Core/RMTabBarController.swift
@@ -39,6 +39,8 @@ final class RMTabBarController: UITabBarController {
             nav.navigationBar.prefersLargeTitles = true
         }
         
+        
+        
         setViewControllers(
             [nav1,nav2,nav3,nav4],
             animated: true
